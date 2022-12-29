3b930defdc8689eb67fc46d923156762d5918eda
nakibuddin <nakibaust137@gmail.com>
2022-12-29 17:11:35 +0600

    mongodb setup added

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = 5000 || process.env.PORT
+
+//middle wire
+app.use(cors());            // for cors policy
+app.use(express.json());    // for post request
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2arvn0y.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run () {
+    try{
+        
+
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.error('my_database_error: ', err));
+
+app.get('/', (req, res) => {
+    res.send('Hello from Task Manager Server');
+})
+
+app.listen(port, () => console.log(`server is running on port ${port}`));
