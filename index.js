f428d54251e4db2bb3defb33a2b23a14cf172ec1
nakibuddin <nakibaust137@gmail.com>
2022-12-30 17:25:23 +0600

    incomplete api added

--- index.js
+++ index.js
@@ -12,16 +12,24 @@ app.use(express.json());    // for post request
 
 
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2arvn0y.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run () {
     try{
-        
+        const taskCollection = client.db('task_manager').collection('tasks');
+
+        app.get('/incomplete',  async(req, res) => {
+            const query = {status: 'incomplete'};
+            const result = await taskCollection.find(query).toArray();
+            res.send(result);
+        })
 
     }
-    finally{
 
+    finally{
+        
     }
 }
 run().catch(err => console.error('my_database_error: ', err));
